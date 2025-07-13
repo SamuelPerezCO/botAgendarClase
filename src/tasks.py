3a7355975b02bf6eb84339f66dd222d7009b3b68
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-07-13 17:52:11 -0500

    Abre el navegador y en la pagina

--- src/tasks.py
+++ src/tasks.py
@@ -1,18 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 import pandas as pd
 import time
 
 # Opciones de navegacion
 options = webdriver.ChromeOptions()
-options.add_argument('--start-maximized')
-options.add_argument('--disable-extensions')
+options.add_argument("--start-maximized")
+options.add_argument("--disable-extensions")
 
-driver_path = 'C:\\APPS\\ChromeDriver\\chromedriver-win64\\chromedriver.exe'
-driver = webdriver.Chrome(driver_path, chrome_options = options)
+driver_path = 'C:\\APPS\\ChromeDriver v138\\chromedriver-win64\\chromedriver.exe'
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service, options=options)
 
-driver.set_window_position(2000 , 0)
+driver.set_window_position(0, 0)   
 driver.maximize_window()
 time.sleep(1)
+
+# Inicializamos el navegador
+driver.get('https://schoolpack.smart.edu.co/idiomas/alumnos.aspx')
+time.sleep(5)
